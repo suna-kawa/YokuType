30c8f0f406a9748e6ccaf021e41d2a3b91bb3bfe
suna-kawa <sunagawa69852@gmail.com>
2023-05-07 22:02:14 +0900

    profileページのview,routes,アクションの大枠を作成,indexページにprofileリンクを設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def profile
+    @user = current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'users#index'
+  devise_for :users, controllers: { :omniauth_callbacks => "users/omniauth_callbacks" }
+  resources :users, only: [:update, :create]
+  get 'users/profile'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
